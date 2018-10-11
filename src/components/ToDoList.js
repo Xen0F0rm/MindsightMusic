20407e6868b1aa1f589809042f924757ec0ba2fb
Caleb Mills <cmills@Calebs-MacBook-Pro.local>
2018-10-11 15:44:27 -0600

    Changed actions index to be a hub file that only exports other files.

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -8,101 +8,107 @@ import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 
 class ToDoList extends Component {
-  state = {
-    addFormVisible: false,
-    addFormValue: ""
-  };
+    state = {
+        addFormVisible: false,
+        addFormValue: "",
+    };
 
-  handleInputChange = event => {
-    this.setState({ addFormValue: event.target.value });
-  };
+    handleInputChange = (event) => {
+        this.setState({ addFormValue: event.target.value });
+    };
 
-  handleFormSubmit = event => {
-    const { addFormValue } = this.state;
-    const { addToDo } = this.props;
+    handleFormSubmit = (event) => {
+        const { addFormValue } = this.state;
+        const { addToDo } = this.props;
 
-    event.preventDefault();
+        event.preventDefault();
 
-    addToDo({ title: addFormValue });
+        addToDo({ title: addFormValue });
 
-    this.setState({ addFormValue: "" });
-  };
+        this.setState({ addFormValue: "" });
+    };
 
-  renderAddForm = () => {
-    const { addFormVisible, addFormValue } = this.state;
+    renderAddForm = () => {
+        const { addFormVisible, addFormValue } = this.state;
 
-    if (addFormVisible) {
-      return (
-        <div>
-          <form noValidate autoComplete="off">
-            <TextField
-              label="Title"
-              value={addFormValue}
-              onChange={this.handleInputChange}
-              type="text"
-            />
-          </form>
-        </div>
-      );
+        if (addFormVisible) {
+            return (
+                <div>
+                    <form
+                        noValidate
+                        autoComplete="off"
+                        onSubmit={this.handleFormSubmit}
+                    >
+                        <TextField
+                            label="Title"
+                            value={addFormValue}
+                            onChange={this.handleInputChange}
+                            type="text"
+                        />
+                    </form>
+                </div>
+            );
+        }
+    };
+
+    renderToDos() {
+        const { data } = this.props;
+        const toDos = _.map(data, (value, key) => {
+            return <ToDoListItem key={key} todoId={key} todo={value} />;
+        });
+
+        if (!_.isEmpty(toDos)) {
+            return toDos;
+        }
+
+        return (
+            <div>
+                <Typography variant="title" gutterBottom>
+                    You have completed all the tasks
+                </Typography>
+
+                <Typography variant="subheading" gutterBottom>
+                    Start by clicking add button in the bottom of the screen
+                </Typography>
+            </div>
+        );
     }
-  };
 
-  renderToDos() {
-    const { data } = this.props;
-    const toDos = _.map(data, (value, key) => {
-      return <ToDoListItem key={key} todoId={key} todo={value} />;
-    });
-
-    if (!_.isEmpty(toDos)) {
-      return toDos;
+    componentWillMount() {
+        this.props.fetchToDos();
     }
 
-    return (
-      <div>
-        <Typography variant="title" gutterBottom>
-          You have completed all the tasks
-        </Typography>
+    // Main render method for component.
+    render() {
+        const { addFormVisible } = this.state;
 
-        <Typography variant="subheading" gutterBottom>
-          Start by clicking add button in the bottom of the screen
-        </Typography>
-      </div>
-    );
-  }
+        return (
+            <div>
+                {this.renderToDos()}
 
-  componentWillMount() {
-    this.props.fetchToDos();
-  }
+                <Button
+                    onClick={() =>
+                        this.setState({ addFormVisible: !addFormVisible })
+                    }
+                    variant="contained"
+                    color="primary"
+                >
+                    {addFormVisible ? "Close" : "Add"}
+                </Button>
 
-  // Main render method for component.
-  render() {
-    const { addFormVisible } = this.state;
-
-    return (
-      <div>
-        {this.renderToDos()}
-
-        <Button
-          onClick={() => this.setState({ addFormVisible: !addFormVisible })}
-          variant="contained"
-          color="primary"
-        >
-          {addFormVisible ? "Close" : "Add"}
-        </Button>
-
-        {this.renderAddForm()}
-      </div>
-    );
-  }
+                {this.renderAddForm()}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = ({ data }) => {
-  return {
-    data
-  };
+    return {
+        data,
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  actions
+    mapStateToProps,
+    actions,
 )(ToDoList);
