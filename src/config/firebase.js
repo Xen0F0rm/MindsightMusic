b64b67baaf1070b4700227ca573e69087435120c
Caleb Mills <cmills@Calebs-MacBook-Pro.local>
2018-10-12 13:54:53 -0600

    Initial commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,12 @@ import * as firebase from "firebase";
 import { FirebaseConfig } from "../config/keys";
 firebase.initializeApp(FirebaseConfig);
 
-export const todosRef = generateEndpoint("todos");
+export const marchingRef = generateEndpoint("marching");
+export const concertRef = generateEndpoint("concert");
+export const supplementalRef = generateEndpoint("supplemental");
+export const newsRef = generateEndpoint("news");
+export const drillRef = generateEndpoint("drill");
+export const aboutRef = generateEndpoint("about");
 
 function generateEndpoint(name) {
     return firebase.database().ref().child(name);
